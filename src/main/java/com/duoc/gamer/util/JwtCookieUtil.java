a13e9fac5647109889151773fec86899efc5a2c7
franpvp <franpvp.98@gmail.com>
2025-04-14 16:34:53 -0400

    Se añade clase de utilidad JwtCookieUtil

--- src/main/java/com/duoc/gamer/util/JwtCookieUtil.java
+++ src/main/java/com/duoc/gamer/util/JwtCookieUtil.java
@@ -5,17 +5,17 @@ import jakarta.servlet.http.Cookie;
 public class JwtCookieUtil {
 
     private static final String COOKIE_NAME = "JWT_TOKEN";
-    private static final int COOKIE_MAX_AGE = 24 * 60 * 60; // 1 día
+    private static final int COOKIE_MAX_AGE = 24 * 60 * 60;
 
     private JwtCookieUtil() {
     }
 
     public static Cookie crearCookieJWT(String jwt) {
         Cookie cookie = new Cookie(COOKIE_NAME, jwt);
-        cookie.setSecure(true);           // solo se envía por HTTPS
-        cookie.setHttpOnly(true);         // no accesible desde JS
-        cookie.setPath("/");              // accesible en toda la app
-        cookie.setMaxAge(COOKIE_MAX_AGE); // duración
+        cookie.setSecure(true);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(COOKIE_MAX_AGE);
         return cookie;
     }
 }
